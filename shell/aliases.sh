4903d75842cffa4f27c32779164ef1238a7dbe70
Pavel <31886681+SpinningVinyl@users.noreply.github.com>
2025-11-09 11:50:24 +0000

    updating Tumbleweed now stops the plymouth service

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,8 +9,14 @@ alias l='ls -CF --group-directories-first'
 alias vim='nvim'
 alias vi='nvim'
 
+# configure emacs for use on the command line
+alias e="emacsclient -tty -a ''"
+
 # update Debian
-alias U='sudo apt update && sudo apt upgrade -y && flatpak update -y'
+# alias U='sudo apt update && sudo apt upgrade -y && flatpak update -y'
+
+# update OpenSUSE
+alias U='sudo systemctl stop plymouth-start.service && sudo zypper refresh && sudo -n zypper dup && sudo flatpak update -y'
 
 # compare two directories
 alias diffdir='diff -rq'
@@ -18,12 +24,9 @@ alias diffdir='diff -rq'
 # sane defaults for bc (Bell calculator)
 alias bc='bc -lq'
 
-# useful aliases for apt-cache
-alias apt-info='apt-cache showpkg'
-
 # force 256 colors in tmux
 alias tmux='tmux -2'
 
 # load tmux sessions
-alias tmm='tmuxp load main'
-alias tmq='tmuxp load aux'
+alias tmm='tmux load main'
+alias tmq='tmux load aux'

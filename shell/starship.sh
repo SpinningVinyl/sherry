62907e8820d486e0dc6f89dc23e40be792e0dddb
Pavel <31886681+SpinningVinyl@users.noreply.github.com>
2025-11-14 12:48:52 +0000

    gitstatus is now unmaintained, switching to starship

---
+++ shell/starship.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+function set_win_title(){
+	echo -ne "\033]0; $(basename "$PWD") \007"
+}
+starship_precmd_user_func="set_win_title"
+
+export STARSHIP_CONFIG="$HOME/.starship.toml"
+eval "$(starship init bash)"
